f9caf25958c02bec6bc12769183376cb98b9b500
Mateo Torres <torresmateo@gmail.com>
2026-01-16 10:33:41 -0300

    Update: Regenerated from updated configuration

---
+++ python_google_adk/main.py
@@ -0,0 +1,74 @@
+from arcadepy import AsyncArcade
+from dotenv import load_dotenv
+from google.adk import Agent, Runner
+from google.adk.artifacts import InMemoryArtifactService
+from google.adk.models.lite_llm import LiteLlm
+from google.adk.sessions import InMemorySessionService, Session
+from google_adk_arcade.tools import get_arcade_tools
+from google.genai import types
+from human_in_the_loop import auth_tool, confirm_tool_usage
+
+import os
+
+load_dotenv(override=True)
+
+
+async def main():
+    app_name = "my_agent"
+    user_id = os.getenv("ARCADE_USER_ID")
+
+    session_service = InMemorySessionService()
+    artifact_service = InMemoryArtifactService()
+    client = AsyncArcade()
+
+    agent_tools = await get_arcade_tools(
+        client, tools=["Gmail_ListEmails", "Gmail_SendEmail"], toolkits=["Slack"]
+    )
+
+    for tool in agent_tools:
+        await auth_tool(client, tool_name=tool.name, user_id=user_id)
+
+    agent = Agent(
+        model=LiteLlm(model=f"openai/{os.environ["OPENAI_MODEL"]}"),
+        name="google_agent",
+        instruction="You're a very useful assistant with access to Gmail and Slack tools, please use them to effectively do tasks requested by the user",
+        description="An agent with Gmail and Slack tools",
+        tools=agent_tools,
+        before_tool_callback=[confirm_tool_usage],
+    )
+
+    session = await session_service.create_session(
+        app_name=app_name, user_id=user_id, state={
+            "user_id": user_id,
+        }
+    )
+    runner = Runner(
+        app_name=app_name,
+        agent=agent,
+        artifact_service=artifact_service,
+        session_service=session_service,
+    )
+
+    async def run_prompt(session: Session, new_message: str):
+        content = types.Content(
+            role='user', parts=[types.Part.from_text(text=new_message)]
+        )
+        async for event in runner.run_async(
+            user_id=user_id,
+            session_id=session.id,
+            new_message=content,
+        ):
+            if event.content.parts and event.content.parts[0].text:
+                print(f'** {event.author}: {event.content.parts[0].text}')
+
+    while True:
+        user_input = input("User: ")
+        if user_input.lower() == "exit":
+            print("Goodbye!")
+            break
+        await run_prompt(session, user_input)
+
+
+if __name__ == '__main__':
+    import asyncio
+    asyncio.run(main())
